c0dbe9fab0f2b70067a3738a57fdaaf90c964c16
ukrwebprom <ukrwebprom@gmail.com>
2023-01-09 21:30:53 +0200

    async-await-upd

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,13 @@
-export const fetchCountries = name => {
+export const fetchCountries = async (name) => {
+  const result = await fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`);
+  if (!result.ok) {
+    throw new Error(result.status);
+  }
+  return await result.json();
+};
+
+
+/* export const fetchCountries = async name => {
   return fetch(
     `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`
   )
@@ -14,4 +23,4 @@ export const fetchCountries = name => {
     .catch(error => {
       throw new Error(error);
     });
-};
+}; */
--- src/index.js
+++ src/index.js
@@ -2,59 +2,54 @@ import './css/styles.css';
 import _ from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
 import Notiflix from 'notiflix';
+import listTpl from './templates/list.hbs';
+import cardTpl from './templates/card.hbs';
 const listContainer = document.querySelector('.country-list');
 const countryContainer = document.querySelector('.country-info');
 const searchBox = document.querySelector('#search-box');
 
 const searchError = () => {Notiflix.Notify.failure('Oops, there is no country with that name');}
-const tooManyResults = () => {Notiflix.Notify.failure('Too many matches found. Please enter a more specific name.');}
+const tooManyResults = () => {Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');}
 const clearScreen = () => {
   listContainer.innerHTML = '';
   countryContainer.innerHTML = '';
 };
 
 const renderList = data => {
-  const list = data
-    .map(item => {
-      return `<li class='country-list__item'><img src="${item.flags.svg}" width="30" height="100%"
-        alt="${item.name}"><p class='country-list__name'>${item.name}</p></li>`;
-    })
-    .join('');
+  const list = data.map(item => {return listTpl({flag:item.flags.svg, name:item.name})}).join('');
   listContainer.innerHTML = list;
 };
 
 const renderCounrty = data => {
-  const countryCard = `<div class='country-card__title'><img src="${data.flags.svg}" width="40" height="100%"
-    alt="${data.name}"><p class='title'>${data.name}</p></div>
-    <p class='country-card__data'>Capital: <span class='country-card__info'>${
-      data.capital
-    }</span></p>
-    <p class='country-card__data'>Population: <span class='country-card__info'>${
-      data.population
-    }</span></p>
-    <p class='country-card__data'>Languages: <span class='country-card__info'>${data.languages
-      .map(lang => lang.name)
-      .join(', ')}</span></p>
-    `;
+  const countryCard = cardTpl({
+    flag: data.flags.svg,
+    name: data.name,
+    capital: data.capital,
+    population: data.population,
+    languages: data.languages.map(lang => lang.name).join(', ')
+  });
   countryContainer.innerHTML = countryCard;
 };
+
+const getCountries = async (evt) => {
+  const request = evt.target.value.trim();
+  if (!request) {
+    return
+  }
+  clearScreen();
+  try {
+    const data = await fetchCountries(request);
+    
+    if (data.length > 10) {tooManyResults();} 
+    else if (data.length > 1) {renderList(data);} 
+    else {renderCounrty(data[0]);}
+  } catch {
+    searchError();
+  }
+}
 
 const DEBOUNCE_DELAY = 300;
 
 
-searchBox.addEventListener(
-  'input',
-  _(evt => {
-    const request = evt.target.value.trim();
-    if (!request) {
-      return
-    }
-    fetchCountries(request).then(data => {
-      clearScreen();
-      if (data.length > 10) {tooManyResults();} 
-      else if (data.length > 1) {renderList(data);} 
-      else {renderCounrty(data[0]);}
-    })
-    .catch(error => searchError());
-  }, DEBOUNCE_DELAY)
-);
+searchBox.addEventListener('input', _(getCountries , DEBOUNCE_DELAY));
+
